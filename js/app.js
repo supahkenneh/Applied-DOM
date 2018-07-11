49fe282545b46b8e903c625d8e9812cc0f9d0b38
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-10 15:52:15 -1000

    button 3

--- js/app.js
+++ js/app.js
@@ -94,6 +94,8 @@
    * the handler method will be a reference to the setMyLightGreen function
    */
 
+   btn3.addEventListener('click', setMyLightGreen);
+
 
   /*
    * Add a click event listener to btn4
