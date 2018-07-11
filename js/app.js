adc3ee5fbcb3c70acc9336f372227bb162fbf1b5
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-10 16:02:34 -1000

    button 8

--- js/app.js
+++ js/app.js
@@ -168,6 +168,10 @@
    *   and passing two aditional arguments, event and 'light-green'
    */
 
+   btn8.addEventListener('click', function(event) {
+     setMyLightClass.call(this, event, 'light-green');
+   })
+
 
   /*
    * Add a click event listener to btn9
